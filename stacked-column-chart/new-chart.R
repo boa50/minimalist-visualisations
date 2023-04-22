0666386c756737ae2be564f7e6ad9835742d9907
boa50 <bruno.albuquerque50@gmail.com>
2023-04-23 05:51:05 +0930

    Separated the theme definition from the chart creation

--- stacked-column-chart/new-chart.R
+++ stacked-column-chart/new-chart.R
@@ -1,9 +1,9 @@
 library(dplyr)
 library(tidyr)
-library(ggplot2)
 library(scales)
+library(ggplot2)
 
-df <- tibble(wharehouse_id = as.character(c(1:19)),
+df <- tibble(warehouse_id = as.character(c(1:19)),
              errors = c(80, 121, 80, 96, 57, 94, 77, 26, 46, 25,
                         45, 48, 30, 11, 34, 39, 16, 22, 14),
              accuracy = c(691, 692, 668, 560, 487, 469, 408, 405, 368, 317,
@@ -14,16 +14,26 @@ df <- tibble(wharehouse_id = as.character(c(1:19)),
   mutate(total = errors + accuracy + nulls,
          accuracy_pct = accuracy / total)
 
-bars_levels <- reorder(df$wharehouse_id, df$accuracy_pct) %>% 
+bars_levels <- reorder(df$warehouse_id, df$accuracy_pct) %>% 
   rev() %>% 
   levels()
 
 df %>% 
   select(1:4) %>% 
-  pivot_longer(!wharehouse_id, names_to = "type", values_to = "value") %>% 
-  ggplot(aes(x = factor(wharehouse_id, levels = bars_levels), 
+  pivot_longer(!warehouse_id, names_to = "type", values_to = "value") %>% 
+  ggplot(aes(x = factor(warehouse_id, levels = bars_levels), 
              y = value, 
              fill = factor(type, levels = c("errors", "nulls", "accuracy")))) +
-  geom_bar(stat = "identity", position = "fill") +
+  geom_bar(stat = "identity", position = "fill", colour = "white") +
   coord_flip() +
-  scale_y_continuous(labels = label_percent()) 
+  scale_y_continuous(labels = label_percent(),
+                     expand = expansion(mult = 0)) +
+  scale_fill_manual(values = c(app_colours$no_emphasis, 
+                               app_colours$no_emphasis2,
+                               app_colours$main)) +
+  labs(x = "Warehouse ID",
+       y = "Total Orders (%)",
+       title = "Most Accurate Warehouses",
+       subtitle = "All wharehouses fulfilled at least 200 orders",
+       caption = "Data from Q4 2021") +
+  theme(legend.position = "top")
---
+++ theme.R
@@ -0,0 +1,44 @@
+### Theme to be used by all visualisations
+library(ggplot2)
+library(ggtext)
+library(showtext)
+font_add_google("Roboto", "roboto")
+showtext_auto()
+
+app_colours <- list(
+  title = "#474747",
+  axis = "#757575",
+  legend_title = "#474747",
+  legend_text = "#757575",
+  subtitle = "#757575",
+  caption = "#8f8f8f",
+  main = "#1976d2",
+  no_emphasis = "#8f8f8f",
+  no_emphasis2 = "#9c9c9c",
+  divergent = "#f57c00",
+  line_main = "#42a5f5",
+  line_complementary = "#78909c"
+)
+
+theme_minimalistic <- function() {
+  theme_classic() +
+    theme(
+      text = element_text(family = "roboto"),
+      plot.title = element_text(hjust = 0, colour = app_colours$title),
+      plot.title.position = "plot",
+      axis.line = element_line(colour = app_colours$axis),
+      axis.ticks = element_line(colour = app_colours$axis),
+      axis.text = element_text(colour = app_colours$axis),
+      axis.title = element_text(colour = app_colours$axis),
+      legend.title = element_text(colour = app_colours$legend_title),
+      legend.text = element_text(colour = app_colours$legend_text),
+      plot.subtitle = element_textbox_simple(colour = app_colours$subtitle,
+                                             margin = margin(b = 15)),
+      plot.caption = element_text(colour = app_colours$caption),
+      plot.caption.position = "plot",
+      panel.background = element_rect(fill = "transparent"),
+      plot.background = element_rect(fill = "transparent", color = NA)
+    )
+}
+
+theme_set(theme_minimalistic())
